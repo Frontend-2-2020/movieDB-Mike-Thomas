bc92da2a6c1e33b07b83ecf188217f97b3991a39
mikelal1 <lalonde.mike@hotmail.com>
2020-01-19 16:00:52 +0100

    Detailpage + Adding Genre and Companylogo components

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,44 +1,81 @@
 import React, { Component } from 'react';
-import {API_KEY, URL_DETAIL} from '../config';
+import { API_KEY, URL_DETAIL } from '../config';
 import axios from 'axios';
+import CompanyLogo from "../components/Companies";
+import Genre from "../components/Genre";
 
 class Detail extends Component {
 
-    state = {};
-
-    componentDidMount(){
+    state = {
+        movie: ''
+    };
+// Hier worden de id's van de movies opgehaald en in movieId gestoken om zo de juiste pagina aan de juiste film
+// te koppelen
+    componentDidMount() {
         const movieId = this.props.match.params.id;
 
-        axios.get(URL_DETAIL + movieId + "?api_key=" + API_KEY)
-        .then(res => {
-            this.setState({movieDetail: res.data});
+        axios.get(URL_DETAIL + movieId + "?api_key=" + API_KEY
+        ).then(response => {
             this.setState({
-                backdrop: "https://image.tmdb.org/t/p/w1280/" + 
-                res.data.backdrop_path
+                movie: response.data
             });
         });
     }
 
-
+// Deze code gebruikt map om over elke movie te gaan en de genre's en companylogo's mee te nemen.
+// En worden dan elk in een component gestoken
     render() {
 
-        const jumbo = {
-            backgroundImage: `url(${this.state.backdrop})`,
-            backgroundSize: "cover",
-            position: "relative"
-        };
+        var nestedRender;
+
+        if (this.state.movie) {
+            nestedRender = (
+                <div>
+                    <div className="mt-6 mb-5">
+                        {this.state.movie.production_companies.map(post => (
+                            <CompanyLogo key={post.id} logo_path={post.logo_path} />
+                        ))}
+                    </div>
+                    <div className="mt-6">
+                        {this.state.movie.genres.map(genre => (
+                            <Genre key={genre.id} genre={genre.name} />
+                        ))}
+                    </div>
+                </div>
+            );
+        }
+
+// Opmaak van de jumbotron en detail pagina
         return (
-            <div className="container">
-                <div style={jumbo} className="jumbotron">
-                    <div style={{
-                            position: "absolute",
-                            top: 0,
-                            left: 0,
-                            width: "100%",
-                            height: "100%",
-                            backgroundColor: "rgba(240,255,255,0.6)"
-                        }}  
-                    />
+            <div style={{ display: "block" }}>
+                <div className="jumbotron" style={{
+                    backgroundImage: `url('https://image.tmdb.org/t/p/original${this.state.movie.backdrop_path}')`
+                }}
+                >
+                    <h1 className="display-4 text-light">
+                        <span>{this.state.movie.title}</span>{" "}
+                        <div className="badge badge-warning" >
+                            {this.state.movie.vote_average}
+                        </div>
+                    </h1>
+                </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-6">
+                            <img src={"https://image.tmdb.org/t/p/w500" + this.state.movie.poster_path} alt="" />
+                        </div>
+                        <div className="col-sm-6">
+                            <div className="card">
+                                <div className="card-body">
+                                    <p className="text-dark">{this.state.movie.overview}</p>
+                                    {nestedRender} 
+                                    <p className="badge badge-primary text-white mt-4">
+                                        Releasedate: {this.state.movie.release_date}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
